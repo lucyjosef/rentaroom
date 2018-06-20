22191f69c1d5666b62f25f1d0815d2959f17b8f5
Batman <luciejoseph@ymail.com>
2018-06-20 17:36:23 +0200

    calendar setup

--- resources/views/list-rooms.blade.php
+++ resources/views/list-rooms.blade.php
@@ -4,6 +4,11 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('css/show-room-list.css') }}">
 @endsection
 
+@section('script')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type="text/javascript" src="{{ asset('js/modal.js') }}"></script>
+@endsection
+
 @section('content')
 <section id="content">
 	<div class="content-result">
@@ -22,8 +27,61 @@
 		        <a href="{{ route('rooms.edit', [$room->id]) }}" class="edit-btn"><i class="fa fa-edit"></i> Edit</a>
 
 		    </div>
+		    <div class="reserve">
+		    	<a href="#" data-action="open-modal"><i class="fab fa-fort-awesome-alt"></i></a>
+		    </div>
 		</div>
 		@endforeach
+	</div>
+	<div data-modal="outside" class="hidden">
+		<div data-modal="inside">
+			<div id="close"><i class="fa fa-times-circle"></i></div>
+			<h1>Get in touch !</h1>
+			<form class="book" method="POST">
+				{{ csrf_field() }}
+				<div class="form-row">
+					{{ Form::label('name', 'You complete name') }}
+					<br>
+					{{ Form::input('text', 'name') }}
+				</div>
+
+				<div class="form-row">
+					{{ Form::label('email', 'Your e-mail') }}
+					<br>
+					{{ Form::input('text', 'email') }}
+				</div>
+
+				<div class="form-row">
+					{{ Form::label('comment', 'Any precision on request') }}
+					<br>
+					{{ Form::textarea('text', 'precisions', ['size' => '50x5']) }}
+				</div>
+
+				<div class="form-row">
+					{{ Form::label('nb-person', 'How many guest') }}
+					<br>
+					{{ Form::input('text', 'nb_person') }}
+				</div>
+
+				<div class="form-row">
+					{{ Form::label('from', 'from') }}
+					<br>
+					{{ Form::date('text', 'from') }}
+				</div>
+
+				<div class="form-row">
+					{{ Form::label('to', 'To') }}
+					<br>
+					{{ Form::date('text', 'to') }}
+				</div>
+
+				<p class="success">{{ session()->get('message') }}</p>
+
+				<div class="form-row">
+					<a data-btn="book" href="#">Submit</a>
+				</div>
+			</form>
+		</div>
 	</div>
 </section>
 @endsection
